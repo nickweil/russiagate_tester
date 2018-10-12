088e9756c38e3dee4a2e3c0780342f76da353059
nickweil <43078610+nickweil@users.noreply.github.com>
2018-10-11 20:56:34 -0700

    update

--- app.js
+++ app.js
@@ -6,24 +6,8 @@ function myfunction() {
 	document.getElementById('demo').style.backgroundColor = "lightblue";
 }
 
-function replacerightside() {
-    var str = document.getElementById("demo").innerHTML; 
-    var res = str.replace(/rid/g, "<span class="redacted">");
-	const escapeRegExp = (string) => {
-		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-	}
-    document.getElementById("demo").innerHTML = res;
-}
-
 function replaceleftside() {
     var str = document.getElementById("demo").innerHTML;
 	var dataArr = str.split("rid")
 	document.getElementById("demo").innerHTML = dataArr[0]+ "<span class="redacted">"+dataArr[1]+"</span>";
 }
-
-
-var txt = "Hello bye";
-var dataArr = txt.split(' ');
-
-var paragraph = document.getElementById("pid");
-paragraph.innerHTML = dataArr[0]+ " <span>"+dataArr[1]+"</span>";
