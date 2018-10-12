444b0b09a4db5e2b72f0f0bb84e191c82598d224
nickweil <43078610+nickweil@users.noreply.github.com>
2018-10-11 21:15:44 -0700

    update

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ function myfunction() {
 function replacesides() {
     var string_in = document.getElementById("myDIV").innerHTML;
 	var first_dataArr = string_in.split("rid")
-	var first_side_done = first_dataArr[0]+ "<span class='redacted'>"+dataArr[1];
+	var first_side_done = first_dataArr[0]+ "<span class='redacted'>"+first_dataArr[1];
 	var second_dataArr = first_side_done.split("rad")
-	var second_side_done = second_dataArr[0]+ "</span>"+dataArr[1];
+	var second_side_done = second_dataArr[0]+ "</span>"+second_dataArr[1];
 	document.getElementById("myDIV").innerHTML = second_side_done;
 }
