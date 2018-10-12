c7ceea6f21e245d1fffbaa642e6969f6cea9e1f3
nickweil <43078610+nickweil@users.noreply.github.com>
2018-10-11 20:23:12 -0700

    update

--- app.js
+++ app.js
@@ -5,3 +5,20 @@
 function myfunction() {
 	document.getElementById('demo').style.backgroundColor = "lightblue";
 }
+
+
+function replaceleftside() {
+    var str = document.getElementById("demo").innerHTML; 
+    var res = str.replace(/[*/g, "<span class="redacted">");
+    document.getElementById("demo").innerHTML = res;
+}
+
+function replacerightside() {
+    var str = document.getElementById("demo").innerHTML; 
+    var res = str.replace(*]/g, "</span>");
+    document.getElementById("demo").innerHTML = res;
+}
+
+"[*" "<span class="redacted">"
+
+"*]" "</span>"
