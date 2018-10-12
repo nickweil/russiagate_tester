851cf7f061c6fad9eb2a72d64288d651d3637439
nickweil <43078610+nickweil@users.noreply.github.com>
2018-10-11 21:13:44 -0700

    update

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ function myfunction() {
 	document.getElementById('demo').style.backgroundColor = "lightblue";
 }
 
-function replaceleftside() {
-    var str = document.getElementById("myDIV").innerHTML;
-	var dataArr = str.split("rid")
-	document.getElementById("myDIV").innerHTML = dataArr[0]+ "<span class='redacted'>"+dataArr[1]+"</span>";
+function replacesides() {
+    var string_in = document.getElementById("myDIV").innerHTML;
+	var first_dataArr = string_in.split("rid")
+	var first_side_done = dataArr[0]+ "<span class='redacted'>"+dataArr[1];
+	var second_dataArr = first_side_done.split("rad")
+	var second_side_done = dataArr[0]+ "</span>"+dataArr[1];
+	document.getElementById("myDIV").innerHTML = second_side_done;
 }
