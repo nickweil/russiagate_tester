1dcde2c273a3ca9aba0b2e24e05ad3b093f6d7ba
nickweil <nick.wheel@gmail.com>
2018-10-15 20:51:18 -0700

    update

--- app.js
+++ app.js
@@ -2,15 +2,12 @@
     document.getElementById("myDIV").style.backgroundColor = "lightblue";
 }*/
 
-function myfunction() {
-	document.getElementById('demo').style.backgroundColor = "lightblue";
-}
 
 function replacesides() {
     var string_in = document.getElementById("myDIV").innerHTML;
-	var first_dataArr = string_in.split("rid")
+	var first_dataArr = string_in.split("[")
 	var first_side_done = first_dataArr[0]+ "<span class='redacted'>"+first_dataArr[1];
-	var second_dataArr = first_side_done.split("rad")
+	var second_dataArr = first_side_done.split("]")
 	var second_side_done = second_dataArr[0]+ "</span>"+second_dataArr[1];
 	document.getElementById("myDIV").innerHTML = second_side_done;
 }
