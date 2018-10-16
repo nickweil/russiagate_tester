be78f3039d598d6044a790e69580e49cee66f441
nickweil <nick.wheel@gmail.com>
2018-10-15 20:56:42 -0700

    update

--- app.js
+++ app.js
@@ -3,11 +3,17 @@
 }*/
 
 
-function replacesides() {
+/*function replacesides() {
     var string_in = document.getElementById("myDIV").innerHTML;
 	var first_dataArr = string_in.split("[")
 	var first_side_done = first_dataArr[0]+ "<span class='redacted'>"+first_dataArr[1];
 	var second_dataArr = first_side_done.split("]")
 	var second_side_done = second_dataArr[0]+ "</span>"+second_dataArr[1];
 	document.getElementById("myDIV").innerHTML = second_side_done;
+}*/
+
+function replacesides() {
+	var string_in = document.getElementById("myDIV").innerHTML;
+	var str = string_in.replace('rid', 'ble');
+	document.getElementById("myDIV").innerHTML = str;
 }
