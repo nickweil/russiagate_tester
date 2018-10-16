508761d12d195414132ccf07a88272709bfc3c34
nickweil <nick.wheel@gmail.com>
2018-10-15 22:29:15 -0700

    update

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ function replacesides() {
 }
 */
 function replacesides() {
-	var x = document.querySelectorAll(/[\/);
+	var x = document.querySelectorAll(/\[/);
     x[0].style.backgroundColor = "red";
 }
