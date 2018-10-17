aa2140e4ce56c93ec6afc43ccd9fd2f9849fe511
nickweil <nick.wheel@gmail.com>
2018-10-16 21:19:33 -0700

    update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function replacesides() {
 	var i;
 	for (i = 0; i < x.length; i++) {
 		x[i].style.backgroundColor = "red";
-		x[i].innerHTML=str.replace(/\[/g, "<span class='redacted'>");
-		x[i].innerHTML=str.replace(/\]/g, "</span>");
+		x[i].innerHTML=str.replace("blah", "bleh");
+		//x[i].innerHTML=str.replace(/\]/g, "</span>");
 	}
 }
