c02f78b95f53b61b7cb336e5cc57fa0a5f656a77
nickweil <43078610+nickweil@users.noreply.github.com>
2019-09-10 16:04:05 -0700

    update

--- app.js
+++ app.js
@@ -44,13 +44,15 @@ search.addWidget(
     container: '#hits',
     templates: {
       item: `
-        <div>
-          <img src="{{senderimgurl}}" align="left" alt="{{sender}}" />
+          <div class="sender">
+            <img src="{{senderimgurl}}" align="left" alt="{{sender}}" />
+          </div>
           <div class="hit-name">
             {{date}} {{weekday}}
           </div>
-		  <img src="{{textimgurl}}" alt="{{contents}}" />
-        </div>
+          <div class="text">
+		    <img src="{{textimgurl}}" alt="{{contents}}" />
+		  </div>
       `,
     },
   })
